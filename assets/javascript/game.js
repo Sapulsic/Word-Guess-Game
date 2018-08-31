b6b9c92b6b63f75c4814ff2d059f23900108b867
Sapulsic <carlos.barahona1995@gmail.com>
2018-08-30 21:35:56 -0500

    made some changes to the code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,23 @@
-Hangman = {
-
+guessingGame = {
+    init: function(words) {
+        guessingGame.words = words,
+        guessingGame.hm = $(".gGame"),
+        guessingGame.message = $(".message"),
+        guessingGame.msgTitle = $(".title"),
+        guessingGame.msgText = $("text"),
+        guessingGame.restart = $(".restart"),
+        guessingGame.wrd = this.randomWord(),
+        guessingGame.correct = 0,   
+        guessingGame.guess = $(".guess"),
+        guessingGame.wrong = $(".wrong"),
+        guessingGame.wrongGuess = [],
+        guessingGame.rightGuess = [],
+        guessingGame.gForm = $(".gForm"),
+        guessingGame.gLetter = $("gLetter"),
+        guessingGame.setup();
+    },
+    
+    
 }
+    
+    var wordList = ["halo", "covenent", "elites", "sanghelios", "master chief", "grunts", "mythic", "jackals", "odst", "arbiter", "prophets", "ark", "flood", "brute", "spartans", "unsc", "blue team"];
