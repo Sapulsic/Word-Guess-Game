fe4ada8f5f70836fe73858cc14aeed88cf86972a
Sapulsic <carlos.barahona1995@gmail.com>
2018-09-05 04:11:25 -0500

    made base js function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,47 @@
-guessingGame = {
-    init: function(words) {
-        guessingGame.words = words,
-        guessingGame.hm = $(".gGame"),
-        guessingGame.message = $(".message"),
-        guessingGame.msgTitle = $(".title"),
-        guessingGame.msgText = $("text"),
-        guessingGame.restart = $(".restart"),
-        guessingGame.wrd = this.randomWord(),
-        guessingGame.correct = 0,   
-        guessingGame.guess = $(".guess"),
-        guessingGame.wrong = $(".wrong"),
-        guessingGame.wrongGuess = [],
-        guessingGame.rightGuess = [],
-        guessingGame.gForm = $(".gForm"),
-        guessingGame.gLetter = $("gLetter"),
-        guessingGame.setup();
-    },
-    
-    
+var wordList = ["halo", "covenent", "elites", "sanghelios", "master chief", "grunts", "mythic", "jackals", "odst", "arbiter", "prophets", "ark", "flood", "brute", "spartans", "unsc", "blue team"];
+
+var count = 0;
+var s;
+var guesses = [];
+
+
+var random = wordList[Math.floor(Math.random() * wordList.length)];
+
+
+function start() {
+    debugger;v
+    for ( var i = 0; i < random.length; i++) {
+        guesses[i] = "_";
+    }
+
+    s = guesses.join(" ");
+    document.getElementById("answer").innerHTML = s;
+    console.log(random);
 }
-    
-    var wordList = ["halo", "covenent", "elites", "sanghelios", "master chief", "grunts", "mythic", "jackals", "odst", "arbiter", "prophets", "ark", "flood", "brute", "spartans", "unsc", "blue team"];
+
+
+function Letters() {
+    debugger;
+    var letter = document.getElementById("gLetter");
+
+
+    if (letter.length > 0) {
+        for (var i = 0; i < random.length; i++) {
+            if (random[i] === letter) {
+                guesses[i] = letter
+            }
+        }
+
+        count++;
+        document.getElementById("wrong").innerHTML = "No. of clicks " + count;
+        document.getElementById("answer").innerHTML = wordList.join(" ");
+
+    }
+    if (count > 9) {
+        document.getElementById("status").innerHTML ="Bruhh, come on!";
+    }
+
+}
+
+
+
